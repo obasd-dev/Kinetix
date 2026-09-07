db7deee71b1f37ec1bea7955f1279ea5f471f6b2
obasd-dev <awotideoluwaseunbukola@gmail.com>
2026-09-07 15:06:37 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Kinetix</h1>
+      <p>Your custom video feed and community app platform.</p>
+    </main>
+  );
+}
